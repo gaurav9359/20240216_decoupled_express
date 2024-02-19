74b779b02c68f4cc030f286643a0a5bea9dbd3de
Gaurav Sanjeev Pathak <114300073+gaurav9359@users.noreply.github.com>
2024-02-19 11:56:02 +0530

    all the json database Functionality added

--- project/index.js
+++ project/index.js
@@ -9,19 +9,84 @@ let gaurav= new Mydatabase("oreno");
 app.use(express.json())
 
 // post the product into the database
-app.post('/', (req, res) => {
+app.post('/product',(req, res) => {
+    try{
     gaurav.createRecord('product',req.body)
-    res.send(gaurav.readRecord("product",4))
+    res.send(gaurav.readRecord("product",req.body.id))
+    }
+    catch(e){
+      res.send(e)
+    }
 })
 
 // put the product into the database
-app.put('/', (req, res) => {
-    gaurav.updateRecord('product',4,req.body)
-    res.send(gaurav.readRecord("product",4))
+app.put('/product', (req, res) => {
+    try{
+    gaurav.updateRecord('product',Number(req.query.id),req.body)
+    res.send(gaurav.readRecord("product",Number(req.query.id)))
+    }
+    catch(e){
+      res.send(e)
+    }
 })
 
-//get the product from database
-app.get('/',(req,re))
+//get product from id
+app.get('/product',(req,res)=>{
+  try{
+    res.send(gaurav.readRecord("product", Number(req.query.id)))
+  }
+  catch(e){
+    res.send(e)
+  }
+})
+
+//Delete product from product json
+app.delete('/product',(req,res)=>{
+  try{
+    let deleted_element= gaurav.readRecord("product",Number(req.query.id))
+    gaurav.deleteRecord('product',Number(req.query.id))
+    res.send(deleted_element)
+  }
+  catch(e){
+    res.send(e)
+  }
+})
+
+//Cancel Purchase
+app.delete('/cancel',(req,res)=>{
+  try{
+    let objectDeleted= gaurav.readRecord('order',Number(req.query.id));
+    gaurav.cancelOrder(Number(req.query.id))
+    res.send(objectDeleted)
+  }
+  catch(e){
+    res.send(e)
+    console.log(e)
+  }
+})
+
+//post the checkout
+app.post('/checkout',(req,res)=>{
+  try{
+    gaurav.checkoutOrder(Number(req.query.id),  req.body)
+    res.send(gaurav.readRecord('order',Number(req.body.id)))
+  }
+  catch(e){
+    res.send(e)
+    console.log(e)
+  }
+})
+
+//get Status of product
+app.get('/status',(req,res)=>{
+  try{
+    res.send(gaurav.getStatus(Number(req.query.id)))
+
+  }
+  catch(e){
+    res.send(e)
+  }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- project/json_implementation/index.js
+++ project/json_implementation/index.js
@@ -5,6 +5,7 @@
  * We need to perform crud operations on all these folder, files and
  * lines
  */
+  const { CANCELLED } = require("dns");
   const fs = require("fs");
   const path = require("path");
   const prompt = require('prompt-sync')();
@@ -237,6 +238,169 @@
         // Success 
         console.log("Deletion Successfull");
       });
+  
+  }
+  
+  /**Checkout the product
+   * @param {Number} productId id of product
+   * @param {Number} quantity  quantity of the product
+   * @throws {Error} if the quantity to purchase > stock available
+   */
+  checkoutOrder(productId,order_details){
+    //take all the products from product.json
+    let allProducts=require(`./database/product.json`)
+    let idArray=[]
+  
+    //populate the id in array
+    for(let singleObject in allProducts){
+      idArray.push(allProducts[singleObject].id)
+    }
+  
+    //if the id of product not found then return error
+    if(!idArray.includes(productId)){
+        console.log("product not found")
+        return
+    }
+  
+    let indexOfProduct=-1
+    //get the index of productId from product
+    for(let key in allProducts){
+      if(allProducts[key].id===productId){
+        indexOfProduct=key
+      }
+    }
+  
+    //check if the stock is available or not
+    if(order_details.quantity>allProducts[indexOfProduct].stock){
+      console.log("insufficient stock available")
+      return
+    }
+  
+    //if the stock is available then reduce the quantity purchased
+    allProducts[indexOfProduct].stock-=order_details.quantity
+  
+    //take all orders from order.json to append it
+    order_details["order_pId"]=productId
+    this.createRecord("order",order_details)
+  
+  
+    //write the product.json file again
+    fs.writeFile(
+      `./dbManagement/database/product.json`,
+      JSON.stringify(allProducts),
+      err => {
+        // Checking for errors 
+        if (err) throw err;
+  
+      });
+  }
+  
+  /**cancel the product
+   * @param {Number} productId id of product
+   */
+  cancelOrder(productId){
+    //take all the products from product.json
+    let allOrders=require(`./database/order.json`)
+    let idArray=[]
+  
+    //populate the id in array
+    for(let singleObject in allOrders){
+      idArray.push(allOrders[singleObject].id)
+    }
+  
+    console.log(idArray)
+    //if the id of product not found then return error
+    if(!idArray.includes(productId)){
+        console.log("product not found")
+        return
+    }
+  
+    let indexOfProduct=-1
+    //get the index of productId from order
+    for(let key in allOrders){
+      if(allOrders[key].id===productId){
+        indexOfProduct=key
+      }
+    }
+    //quantity to add in the product json
+    let quantityToAdd=allOrders[indexOfProduct].quantity
+    let objectDeleted= allOrders[indexOfProduct]
+  
+    //delete the indexOfProduct form order
+    allOrders.splice(indexOfProduct,1)
+    
+    //search for index of product that to be deleted in productjson
+    let allProducts=require(`./database/product.json`)
+  
+    for(let key in allProducts){
+      if(allProducts[key].id===productId){
+        indexOfProduct=key
+      }
+    }
+    allProducts[indexOfProduct].stock+=quantityToAdd
+  
+    //write in order.json file again
+    fs.writeFile(
+      `./dbManagement/database/order.json`,
+      JSON.stringify(allOrders),
+      err => {
+        // Checking for errors 
+        if (err) throw err;
+  
+      });
+  
+    //write the product.json file again
+    fs.writeFile(
+      `./dbManagement/database/product.json`,
+      JSON.stringify(allProducts),
+      err => {
+        // Checking for errors 
+        if (err) throw err;
+  
+      });
+  
+      //send the order cancelled into the call_order.json
+      let allCancelled= require(`./database/cancelled_order.json`)
+      objectDeleted.order_status="Cancelled"
+      allCancelled.push(objectDeleted)
+  
+      //write into the json again
+      fs.writeFile(
+        `./dbManagement/database/cancelled_order.json`,
+        JSON.stringify(allCancelled),
+        err => {
+          // Checking for errors 
+          if (err) throw err;
+    
+        });
+  
+  }
+  
+  /** To get the status of the product purchased
+   * @param {Number} productId 
+   * @returns {Object} having status
+   */
+  getStatus(productId){
+    // check if the product is CANCELLED
+    let allCancelled= require(`./database/cancelled_order.json`)
+  
+    for(let key in allCancelled){
+      if(allCancelled[key].id==productId){
+        return allCancelled[key]
+      }
+    }
+  
+    //check if the product is placed
+    let allPlaced= require(`./database/order.json`)
+  
+    for(let key in allPlaced){
+      if(allPlaced[key].id==productId){
+        return allPlaced[key]
+      }
+    }
+  
+    // if the order is not made or cancelled
+    console.log("no such order have been made or cancelled")
   
   }
   }
